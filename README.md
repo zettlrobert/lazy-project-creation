a1bc5900e835a417ecdc868d50ed9ded47d118be
Robert Zettl <zettl.robert@gmail.com>
2020-02-23 21:39:15 +0100

    added inquirer and test prompt

---
+++ README.md
@@ -0,0 +1,6 @@
+## Dependencies
+* inquirer
+
+To publish
+
+    $ now publish
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+const inquire = require('inquirer');
+
+
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+      type: 'confirm',
+      name: 'isHuman',
+      message: 'Are you a human?',
+      default: false
+    }
+  ])
+  .then(answers => {
+    console.log(answers);
+    // Use user feedback for... whatever!!
+  });
+
 console.log("First log");
